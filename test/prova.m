38c88e3c24d5925cfd3daa00ba78506a6a442cde
PaoloFori <forin.paolo98@gmail.com>
2024-07-01 17:03:18 +0200

    test if equal to matlab

---
+++ test/prova.m
@@ -0,0 +1,82 @@
+clc; clear all; close all;
+% create the dataset log band power, all channels and freq 8-14 ---> uqual
+% to main4d
+
+start = 1;
+
+%% process throught matlab
+% not modification needed for these informations
+sampleRate = 512;
+filein = '/home/paolo/cvsa_ws/src/processing_cvsa/test/rawdata.csv';
+data = readmatrix(filein);
+filterOrder = 4;
+band = [8, 14];
+bufferSize = 512;
+frameSize = 32;
+
+[s_buffer, s_band, s_pow, s_avg, s_log] = online_rosneuro(data, bufferSize, frameSize, band, filterOrder, sampleRate);
+
+
+%% Load file of rosneuro
+channelId = 1;
+SampleRate = 512;
+
+files{1} = '/home/paolo/cvsa_ws/src/processing_cvsa/test/bandpass.csv';
+files{2} = '/home/paolo/cvsa_ws/src/processing_cvsa/test/pow.csv';
+files{3} = '/home/paolo/cvsa_ws/src/processing_cvsa/test/avg.csv';
+files{4} = '/home/paolo/cvsa_ws/src/processing_cvsa/test/log.csv';
+files{5} = '/home/paolo/cvsa_ws/src/processing_cvsa/test/buffer.csv';
+
+for i=1:length(files)
+    file = files{i};
+    rosneuro_data = readmatrix(file);
+    if i == 1
+        matlab_data = s_band;
+        c_title = "butterworth";
+        nsamples = size(matlab_data,1);
+        t = 0:1/SampleRate:nsamples/SampleRate - 1/SampleRate;
+    elseif i == 2
+        matlab_data = s_pow;
+        c_title = "power";
+        nsamples = size(matlab_data,1);
+        t = 0:1/SampleRate:nsamples/SampleRate - 1/SampleRate;
+    elseif i ==3
+        matlab_data = s_avg;
+        c_title = "avg";
+        nsamples = size(matlab_data,1);
+        t = 0:1/SampleRate:nsamples/SampleRate - 1/SampleRate;
+    elseif i ==4
+        matlab_data = s_log;
+        c_title = "log";
+        nsamples = size(matlab_data,1);
+        t = 0:1/SampleRate:nsamples/SampleRate - 1/SampleRate;
+    else
+        matlab_data = s_buffer;
+        c_title = "buffer";
+        nsamples = size(matlab_data,1);
+        t = 0:1/SampleRate:nsamples/SampleRate - 1/SampleRate;
+    end
+
+    figure;
+    subplot(2, 1, 1);
+    hold on;
+    plot(t(start:end), rosneuro_data(start:end, channelId), 'b', 'LineWidth', 1);
+    plot(t(start:end), matlab_data(start:end, channelId), 'r');
+    legend('rosneuro', 'matlab');
+    hold off;
+    grid on;
+
+    subplot(2,1,2)
+    bar(t(start:end), abs(rosneuro_data(start:end, channelId)- matlab_data(start:end, channelId)));
+    grid on;
+    xlabel('time [s]');
+    ylabel('amplitude [uV]');
+    title('Difference')
+
+    sgtitle(['Evaluation' c_title])
+end
+
+
+
+
+
---
+++ test/test_CVSA.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <string>
+#include <ros/ros.h>
+#include <eigen3/Eigen/Dense>
+#include <fstream>
+#include <rosneuro_filters_butterworth/Butterworth.hpp>
+#include <rosneuro_buffers_ringbuffer/RingBuffer.hpp>
+
+template<typename T>
+void writeCSV(const std::string& filename, const Eigen::Ref<const Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>>&  matrix) {
+	const static Eigen::IOFormat format(Eigen::FullPrecision, Eigen::DontAlignCols, ", ", "\n");
+
+	std::ofstream file(filename);
+	if (file.is_open()) {
+		file << matrix.format(format);
+		file.close();
+	}
+}
+
+template<typename T>
+Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> readCSV(const std::string& filename) {
+
+	std::vector<T> values;
+
+	std::ifstream file(filename);
+	std::string row;
+	std::string entry;
+	int nrows = 0;
+
+	while (getline(file, row)) {
+		std::stringstream rowstream(row);
+
+		while (getline(rowstream, entry, ',')) {
+			values.push_back(std::stod(entry));
+		}
+		nrows++; 
+	}
+
+	return Eigen::Map<Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(values.data(), nrows, values.size() / nrows);
+
+}
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "test_CVSA");
+    std::string datapath = "/home/paolo/cvsa_ws/src/processing_cvsa";
+    const std::string fileinput  = datapath + "/test/rawdata.csv";
+    const std::string fileoutput_bandpass  = datapath + "/test/bandpass.csv";
+    const std::string fileoutput_pow  = datapath + "/test/pow.csv";
+    const std::string fileoutput_avg  = datapath + "/test/avg.csv";
+    const std::string fileoutput_log  = datapath + "/test/log.csv";
+    const std::string fileoutput_buffer  = datapath + "/test/buffer.csv";
+
+    // Load input data
+    ROS_INFO("Loading data from %s", fileinput.c_str());
+	Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> input = readCSV<double>(fileinput);
+    int frameSize = 32;
+    int nchannels = input.cols();
+    int nsamples  = input.rows();
+    int bufferSize = 512;
+    int size_band_pow = ((nsamples/frameSize)-(bufferSize/frameSize - 1))*bufferSize;
+    int size_avg_log = (nsamples/frameSize) - (bufferSize/frameSize - 1);
+
+    // Configure filters
+    ROS_INFO("Configure filters");
+    int filterOrder = 4;
+    float sampleRate = 512.0;
+    std::vector<double> band = {8.0, 14.0};
+    rosneuro::Butterworth<double> low_pass(rosneuro::ButterType::LowPass,  filterOrder,  band.at(1), sampleRate);
+    rosneuro::Butterworth<double> high_pass(rosneuro::ButterType::HighPass,  filterOrder,  band.at(0), sampleRate);
+    rosneuro::Buffer<double>* buffer = new rosneuro::RingBuffer<double>();
+    buffer->configure("RingBufferCfg");
+
+    // Allocate time variables for time analysis
+	ros::WallTime start_loop, stop_loop;
+    std::vector<double> time_loop;
+
+    // Simulate a rosneuro loop
+    rosneuro::DynamicMatrix<double> data;
+    Eigen::MatrixXd buffer_data = Eigen::MatrixXd::Zero(size_band_pow, nchannels);
+    rosneuro::DynamicMatrix<double> bandpass = rosneuro::DynamicMatrix<double>::Zero(size_band_pow, nchannels);
+    Eigen::MatrixXd pow = Eigen::MatrixXd::Zero(size_band_pow, nchannels);
+    Eigen::MatrixXd avg = Eigen::MatrixXd::Zero(size_avg_log, nchannels);
+    Eigen::MatrixXd log = Eigen::MatrixXd::Zero(size_avg_log, nchannels);
+
+    ROS_INFO("Start simulated loop");
+	auto count = 0;
+    for(auto i = 0; i<nsamples; i = i+frameSize) {
+
+        buffer->add(input.middleRows(i, frameSize));
+        if(!buffer->isfull()) {
+            ROS_WARN("Buffer not full");
+            continue;
+        }
+        start_loop = ros::WallTime::now();
+
+		data = buffer->get();
+        buffer_data.middleRows(count*bufferSize, bufferSize) = data;
+        Eigen::MatrixXd data_low, data_band, data_pow, data_avg, data_log;
+		
+        ROS_INFO("Applying bandpass filter");
+		data_low = low_pass.apply(data);
+        data_band = high_pass.apply(data_low);
+        bandpass.middleRows(count*bufferSize, bufferSize) = data_band;
+
+        ROS_INFO("Applying power");
+        data_pow = data_band.array().pow(2);
+        pow.middleRows(count*bufferSize, bufferSize) = data_pow;
+
+        ROS_INFO("Applying average");
+        data_avg = data_pow.colwise().mean();
+        avg.middleRows(count, 1) = data_avg;
+
+        ROS_INFO("Applying log");
+        data_log = data_avg.array().log();
+        log.middleRows(count, 1) = data_log;
+
+		count++;
+        stop_loop = ros::WallTime::now();
+
+        time_loop.push_back((stop_loop-start_loop).toNSec()/1000.0f);
+	}
+    
+    Eigen::VectorXd time_loop_eigen = Eigen::Map<Eigen::VectorXd>(time_loop.data(), time_loop.size());
+	
+	ROS_INFO("Loop ended: filters applied on data");
+	ROS_INFO("Overall loop time (%d iterations): %f ms", count, time_loop_eigen.mean()); 
+    
+
+    ROS_INFO("Saving data");
+    writeCSV<double>(fileoutput_buffer, buffer_data);
+    writeCSV<double>(fileoutput_bandpass, bandpass);
+    writeCSV<double>(fileoutput_pow, pow);
+    writeCSV<double>(fileoutput_avg, avg);
+    writeCSV<double>(fileoutput_log, log);
+
+    ros::shutdown();
+    return 0;
+}
